4a7967c3d141b36551e032f41d296f46e42177f2
NickDastley <ni.dilger@gmail.com>
2024-05-29 18:22:34 +0200

    added goal_attribute and comments

--- Coursework_Problems/12.6-Exam_bonus_problem.py
+++ Coursework_Problems/12.6-Exam_bonus_problem.py
@@ -1,5 +1,7 @@
 import math
 
+goal_attribute = "drink"
+
 attributes = {
     "thirsty", "nauseous", "hot"
 }
@@ -37,13 +39,20 @@ examples = [
 ]
 
 
-# selects most common output value among set of examples
-# returns True or False
 def plurality_val(examples):
+    """
+    Calculates the majority value in a set of examples based on the goal_attribute key.
+
+    Parameters: examples (list): A list of dictionaries representing examples. Each dictionary should have a the
+    goal_attribute key with a boolean value.
+
+    Returns:
+        bool: True if the majority value is True, False otherwise.
+    """
     count_true = 0
     count_false = 0
     for example in examples:
-        if example["drink"]:
+        if example[goal_attribute]:
             count_true += 1
         else:
             count_false += 1
@@ -51,42 +60,41 @@ def plurality_val(examples):
 
 
 def all_examples_same_classification(examples):
-    first_classification = examples[0]["drink"]
+    first_classification = examples[0][goal_attribute]
     for i in range(1, len(examples)):
-        if examples[i]["drink"] != first_classification:
+        if examples[i][goal_attribute] != first_classification:
             return False
     return True
 
 
 # Returns the attribute with the highest information gain
-def binary_entropy(attribute, examples):
+def binary_entropy_for_attribute(attribute, examples):
     true_probability = len([example for example in examples if example[attribute]]) / len(examples)
-    false_probability = 1 - true_probability
-    return - (true_probability * math.log2(true_probability) + false_probability * math.log2(false_probability))
+    return binary_entropy(true_probability)
 
 
-def binary_entropy_k(probability_k):
+def binary_entropy(probability_k):
     if probability_k == 0 or probability_k == 1:
         return 0
     return - (probability_k * math.log2(probability_k) + (1 - probability_k) * math.log2(1 - probability_k))
 
 def remainder(attribute, examples):
     option_1_examples = [example for example in examples if example[attribute]]
-    option_1_probability = len([example for example in option_1_examples if example["drink"]]) / len(examples)
+    option_1_probability = len([example for example in option_1_examples if example[goal_attribute]]) / len(examples)
     option_2_examples = [example for example in examples if not example[attribute]]
-    option_2_probability = len([example for example in option_2_examples if example["drink"]]) / len(examples)
+    option_2_probability = len([example for example in option_2_examples if example[goal_attribute]]) / len(examples)
 
     remainder_1 = (len(option_1_examples) / len(examples)
-                   * binary_entropy_k(option_1_probability / len(option_1_examples)))
+                   * binary_entropy(option_1_probability / len(option_1_examples)))
 
     remainder_2 = (len(option_2_examples) / len(examples)
-                   * binary_entropy_k(option_2_probability / len(option_2_examples)))
+                   * binary_entropy(option_2_probability / len(option_2_examples)))
 
     return remainder_1 + remainder_2
 
 
 def information_gain(attribute, examples):
-    return binary_entropy(attribute, examples) - remainder(attribute, examples)
+    return binary_entropy_for_attribute(attribute, examples) - remainder(attribute, examples)
 
 
 def best_information_gaining_attribute(attributes, examples):
@@ -98,11 +106,15 @@ def best_information_gaining_attribute(attributes, examples):
 
 
 def decision_tree_learning(examples, attributes, parent_examples):
+    # ran out of examples
     if len(examples) == 0:
         return plurality_val(parent_examples)
+
     elif all_examples_same_classification(examples):
         # return the classification
-        return examples[0]["drink"]
+        return examples[0][goal_attribute]
+
+    # no attributes remaining
     elif len(attributes) == 0:
         return plurality_val(examples)
     else:
@@ -144,17 +156,16 @@ class Node:
         else:
             self.right = node
 
-    def print_illustration(self):
-        print(self.label)
+    def print_illustration(self, indent=""):
+        print(indent + self.label)
         if isinstance(self.left, Node):
-            self.left.print_illustration()
+            self.left.print_illustration(indent + "├── ")
         else:
-            print(self.left)
+            print(indent + "├── " + str(self.left))
         if isinstance(self.right, Node):
-            self.right.print_illustration()
+            self.right.print_illustration(indent + "└── ")
         else:
-            print(self.right)
-        pass
+            print(indent + "└── " + str(self.right))
 
 
 tree = decision_tree_learning(examples, attributes, examples)
