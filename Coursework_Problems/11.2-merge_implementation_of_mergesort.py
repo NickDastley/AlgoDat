a46e4f5d317fb50560e9978440e0a0a6ff7f6bb7
NickDastley <ni.dilger@gmail.com>
2024-07-07 16:34:33 +0200

    new merge implementation

--- Coursework_Problems/11.2-merge_implementation_of_mergesort.py
+++ Coursework_Problems/11.2-merge_implementation_of_mergesort.py
@@ -12,31 +12,54 @@ def merge_sort(S):
 
 
 def merge(S1, S2, S):
-    S_iterator = -1
-    S1_iterator = 0
-    S2_iterator = 0
+    i = j = k = 0
+    while i < len(S1) and j < len(S2):
+        if S1[i] < S2[j]:
+            S[k] = S1[i]
+            i += 1
+        else:
+            S[k] = S2[j]
+            j += 1
+        k += 1
 
-    for i in range(len(S)):
-        S_iterator += 1
-        if S1_iterator >= len(S1):
-            S[S_iterator] = S2[S2_iterator]
-            S2_iterator += 1
-            continue
+    while i < len(S1):
+        S[k] = S1[i]
+        i += 1
+        k += 1
 
-        if S2_iterator >= len(S2):
-            S[S_iterator] = S1[S1_iterator]
-            S1_iterator += 1
-            continue
+    while j < len(S2):
+        S[k] = S2[j]
+        j += 1
+        k += 1
 
-        if S1[S1_iterator] <= S2[S2_iterator]:
-            S[S_iterator] = S1[S1_iterator]
-            S1_iterator += 1
-            continue
 
-        if S1[S1_iterator] > S2[S2_iterator]:
-            S[S_iterator] = S2[S2_iterator]
-            S2_iterator += 1
-            continue
-    return
+#
+# def merge(S1, S2, S):
+#     S_iterator = -1
+#     S1_iterator = 0
+#     S2_iterator = 0
+#
+#     for i in range(len(S)):
+#         S_iterator += 1
+#         if S1_iterator >= len(S1):
+#             S[S_iterator] = S2[S2_iterator]
+#             S2_iterator += 1
+#             continue
+#
+#         if S2_iterator >= len(S2):
+#             S[S_iterator] = S1[S1_iterator]
+#             S1_iterator += 1
+#             continue
+#
+#         if S1[S1_iterator] <= S2[S2_iterator]:
+#             S[S_iterator] = S1[S1_iterator]
+#             S1_iterator += 1
+#             continue
+#
+#         if S1[S1_iterator] > S2[S2_iterator]:
+#             S[S_iterator] = S2[S2_iterator]
+#             S2_iterator += 1
+#             continue
+#     return
 
-print(merge_sort([3,6,2,4,7,2,6,7]))
+print(merge_sort([3, 6, 2, 4, 7, 2, 6, 7]))
